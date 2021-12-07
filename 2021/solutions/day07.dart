66d18673f5aac0fb683d44afa1fcb3d6d04d2dcb
Maks Kolman <maks@kolman.si>
2021-12-07 10:01:37 +0100

    Improve day07

--- 2021/solutions/day07.dart
+++ 2021/solutions/day07.dart
@@ -1,35 +1,50 @@
 import 'dart:io';
 import 'dart:math';
 
-int fuelUsage(List<int> nums, int pos) {
-  int result = 0;
-  for (final n in nums) {
-    result += max(pos-n, n-pos);
-  }
-  return result;
-}
+int solve(List<int> nums, bool easy) {
+  // leftCost[d] tells us how much it costs for crabs to the left of d to move to d
+  var leftCost = List<int>.filled(nums.last - nums[0] + 1, 0);
+  // rightCost[d] tells us how much it costs for crabs to the right of d to move to d
+  var rightCost = List<int>.filled(nums.last - nums[0] + 1, 0);
 
-int fuelUsage2(List<int> nums, int pos) {
-  int result = 0;
-  for (final n in nums) {
-    int v = max(pos-n, n-pos);
-    result += v * (v+1) ~/ 2;
+  // Fill leftCost
+  var many = 0;
+  var moveCost = 0;
+  for (var d = 0; d < leftCost.length; d++) {
+    if (d > 0) {
+        leftCost[d] = leftCost[d-1] + moveCost;
+    }
+    while (d < leftCost.length - 1 && nums[many] == nums[0] + d) {
+      many += 1;
+    }
+    moveCost = easy ? many : moveCost + many;
   }
-  return result;
+
+  // Fill rightCost
+  many = 0;
+  moveCost = 0;
+  for (var d = rightCost.length-1; d >= 0; d--) {
+    if (d < rightCost.length - 1) {
+        rightCost[d] = rightCost[d+1] + moveCost;
+    }
+    while (d > 0 && nums[nums.length - 1 - many] == nums[0] + d) {
+      many += 1;
+    }
+    moveCost = easy ? many : moveCost + many;
+  }
+
+  // Find min leftCost[d]+rightCost[d]
+  many = rightCost[0] + leftCost[0];
+  for (var d = 0; d < leftCost.length; d++) {
+    many = min(many, rightCost[d] + leftCost[d]);
+  }
+  return many;
 }
 
 void main() {
   final nums = stdin.readLineSync()!.split(',').map(int.parse).toList();
   nums.sort();
-
-  num minFuel = fuelUsage(nums, 0);
-  num minFuel2 = fuelUsage2(nums, 0);
-  for (var i = nums[0]; i <= nums.last; i++) {
-    minFuel = min(minFuel, fuelUsage(nums, i));
-    minFuel2 = min(minFuel2, fuelUsage2(nums, i));
-  }
-
-  print(minFuel);
-  print(minFuel2);
+  print(solve(nums, true));
+  print(solve(nums, false));
 }
 
